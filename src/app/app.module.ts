7d4c3e4c25181b6cdbe2fd03b62be45235e6f650
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-11-03 19:03:26 -0500

    Wade's Pool Cleaning 11-3-22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { HeaderComponent } from './header/header.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
