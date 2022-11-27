85182e194f717603e535dbdb61e1d489b55a9d3d
theGeekkit <sonofgondolin16@gmail.com>
2022-11-26 20:23:32 -0600

    11-26-22

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
@@ -12,8 +12,25 @@ export class AuthComponent implements OnInit {
   constructor(private authService: AuthService, private router:
     Router, private formBuilder: FormBuilder ) { }
 
-    ngOnInit() {
+    authForm: FormGroup;
+    isSubmitted = false;
 
+    ngOnInit() {
+      this.authForm = this.formBuilder.group({
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+      });
     }
+
+    get formControls() { return this.authForm.controls; }
+
+  signIn(){
+    this.isSubmitted = true;
+    if(this.authForm.invalid){
+      return;
+    }
+    this.authService.signIn(this.authForm.value);
+    this.router.navigateByUrl('/admin');
+  }
 
 }
